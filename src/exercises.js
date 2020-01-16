025a1365be0ae88713242e9bf8b22cc8f5f417a9
Nirayka Monga <NMonga@ucvts.tec.nj.us>
2020-01-16 10:45:41 -0500

    well .

--- src/exercises.js
+++ src/exercises.js
@@ -3,15 +3,49 @@ function commonEnd(a, b) {
     return false;
   }
 
-  // write your code here
+  let firstValue1 = a[0]
+  let lastValue1 = a[a.length - 1]
+  let firstValue2 = b[0]
+  let lastValue2 = b[b.length-1]
+
+  if (firstValue1 === firstValue2 || lastValue1 === lastValue2) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function endsMeet(values, n) {
-  // write your code here
+  var newArray = []
+  var arrayLimit = (values.length - 1 - n)
+  if (values === undefined || values.length < n || n < 0 || Number.isInteger(n)) {
+    return newArray;
+  } else {
+    for (i = 0; i < n; i++) {
+      let beginningValue = values[i]
+      newArray.push(beginningValue)
+    }
+    for (i = n - 1; i > arrayLimit; i--) {
+      let thisValue = values[i]
+      newArray.push(thisValue)
+    }
+  }
 }
 
 function difference(numbers) {
-  // write your code here
+  function checkNumber(aNumber) {
+    return Number.isInteger(aNumber)
+  }
+  if (numbers == undefined || numbers.length < 1 || { // check to see if is a number )
+    return undefined;
+  } else {
+    let ascending = numbers.sort(function(a, b){return a - b});
+    let smallest = numbers[0]
+    let descending = numbers.sort(function(a, b){return b - a});
+    let largest = numbers[0]
+    let difference = largest - smallest;
+    return difference;
+  }
 }
 
 function max(number) {
